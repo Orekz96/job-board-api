3890aa18b520bf14aedd6b13031e8476cf2cca5b
Richard Orekoya <orekoyadare12@gmail.com>
2024-10-10 21:49:01 +0100

    Initiate integration with frontend

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import "reflect-metadata";
-import dotenv from "dotenv";
-import app from "./app";
-import config from "./config";
-import log from "./utils/logger";
-import { initializeMongoDataSource } from "./data-source";
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import app from './app';
+import config from './config';
+import log from './utils/logger';
+import { initializeMongoDataSource } from './data-source';
 
 dotenv.config();
 
@@ -11,12 +11,12 @@ const port = config.port;
 
 // Initialize MongoDB Data Source and Start Server
 initializeMongoDataSource()
-  .then(() => {
-    app.listen(port, () => {
-      log.info(`Server is listening on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    log.error("Error initializing MongoDB connection:", error);
-    process.exit(1); // Exit the process if MongoDB connection fails
-  });
+	.then(() => {
+		app.listen(port, () => {
+			log.info(`Server is listening on port ${port}`);
+		});
+	})
+	.catch((error) => {
+		log.error('Error initializing MongoDB connection:', error);
+		process.exit(1); // Exit the process if MongoDB connection fails
+	});
